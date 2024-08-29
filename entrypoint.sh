33cd465ceb2a1359535843386473643a41fd5b97
SriramVS <sriram.sekar@zohocorp.com>
2024-08-29 16:21:20 +0530

    Update entrypoint.sh
    Updating new agent archive release 19.8.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -179,7 +179,7 @@ x=1
 while [ $x -le 60 ]
 do
 	DOMAIN=`domain_decider`
-        wget $DOMAIN/server/Site24x7MonitoringAgent.install
+        wget $DOMAIN/server/archive/linux/1980/Site24x7MonitoringAgent.install
         if [ $? = 0 ]
         then
                 constructInstallationParam
